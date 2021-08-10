1d7a62cd24dbadf063849777d31e217b1763e4d4
Chirag Trivedi <chirag.trivedi@gmail.com>
2021-08-09 20:30:51 -0400

    a dynamic music store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Routing from './component/routing';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Routing/>,
   document.getElementById('root')
 );
 
